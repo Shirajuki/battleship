f12460162efc367238249a7224362592ad0c268b
Shirajuki <shirajuki00@gmail.com>
2022-08-20 20:40:34 +0200

    feat: update battleship assets

--- battleship-backend/models/player.js
+++ battleship-backend/models/player.js
@@ -1,12 +1,81 @@
 import { PlayerBoard, EnemyBoard } from "./board.js";
-import { Ship, BigShip, SmallShip } from "./ship.js";
+import {
+  TwoShipEnded,
+  ThreeShipEnded,
+  BigLShip,
+  LargeLShip,
+  ThreeShip,
+  TShip,
+  BigZShip,
+  LShip,
+  BigLShipMirror,
+  LargeTShip,
+  ZShip,
+  FiveShip,
+  FourShipEnded,
+  FourShip,
+} from "./ship.js";
+
+const getRandomBuild = () => {
+  const rnd = Math.floor(Math.random() * 6);
+  switch (rnd) {
+    case 0:
+      return [
+        new TwoShipEnded(),
+        new TwoShipEnded(),
+        new ThreeShipEnded(),
+        new BigLShip(),
+        new LargeLShip(),
+      ];
+    case 1:
+      return [
+        new TwoShipEnded(),
+        new TwoShipEnded(),
+        new ThreeShip(),
+        new TShip(),
+        new BigZShip(),
+      ];
+    case 2:
+      return [
+        new TwoShipEnded(),
+        new TwoShipEnded(),
+        new LShip(),
+        new BigLShipMirror(),
+        new LargeTShip(),
+      ];
+    case 3:
+      return [
+        new TwoShipEnded(),
+        new TwoShipEnded(),
+        new LShip(),
+        new ZShip(),
+        new FiveShip(),
+      ];
+    case 4:
+      return [
+        new TwoShipEnded(),
+        new ThreeShipEnded(),
+        new ThreeShip(),
+        new FourShipEnded(),
+        new FiveShip(),
+      ];
+    default:
+      return [
+        new TwoShipEnded(),
+        new TwoShipEnded(),
+        new LShip(),
+        new FourShip(),
+        new LargeTShip(),
+      ];
+  }
+};
 
 class Player {
   constructor(id) {
     this.id = id;
     this.playerBoard = new PlayerBoard(10, 10);
     this.enemyBoard = new EnemyBoard(10, 10);
-    this.ships = [new Ship(), new BigShip(), new SmallShip()];
+    this.ships = getRandomBuild();
     this.shots = [];
     this.placing = true;
   }
--- battleship-backend/models/ship.js
+++ battleship-backend/models/ship.js
@@ -2,7 +2,7 @@ export class Ship {
   constructor() {
     this.placed = false;
     this.pos = { x: -5, y: -5 };
-    this.parts = [{ x: 0, y: 0, shot: false }];
+    this.parts = [{ x: 0, y: 0, shot: false, sprite: { x: 0, y: 0 } }];
   }
   place(x, y) {
     this.pos = { x: x, y: y };
@@ -24,23 +24,193 @@ export class Ship {
   }
 }
 
-export class BigShip extends Ship {
+// -=
+export class TwoShipEnded extends Ship {
   constructor() {
     super();
     this.parts = [
-      { x: -1, y: 0, shot: false },
-      { x: 0, y: 0, shot: false },
-      { x: 1, y: 0, shot: false },
+      { x: -1, y: 0, shot: false, sprite: { x: 1, y: 0 } },
+      { x: 0, y: 0, shot: false, sprite: { x: 0, y: 1 } },
     ];
   }
 }
-
-export class SmallShip extends Ship {
+// -==
+export class ThreeShipEnded extends Ship {
   constructor() {
     super();
     this.parts = [
-      { x: 0, y: 0, shot: false },
-      { x: 1, y: 0, shot: false },
+      { x: -1, y: 0, shot: false, sprite: { x: 1, y: 0 } },
+      { x: 0, y: 0, shot: false, sprite: { x: 3, y: 0 } },
+      { x: 1, y: 0, shot: false, sprite: { x: 0, y: 1 } },
+    ];
+  }
+}
+// -=-
+export class ThreeShip extends Ship {
+  constructor() {
+    super();
+    this.parts = [
+      { x: -1, y: 0, shot: false, sprite: { x: 1, y: 0 } },
+      { x: 0, y: 0, shot: false, sprite: { x: 3, y: 0 } },
+      { x: 1, y: 0, shot: false, sprite: { x: 1, y: 1 } },
+    ];
+  }
+}
+//  l
+// -+
+export class LShip extends Ship {
+  constructor() {
+    super();
+    this.parts = [
+      { x: -1, y: 0, shot: false, sprite: { x: 1, y: 0 } },
+      { x: 0, y: 0, shot: false, sprite: { x: 2, y: 3 } },
+      { x: 0, y: -1, shot: false, sprite: { x: 0, y: 3 } },
+    ];
+  }
+}
+// -===
+export class FourShipEnded extends Ship {
+  constructor() {
+    super();
+    this.parts = [
+      { x: -1, y: 0, shot: false, sprite: { x: 1, y: 0 } },
+      { x: 0, y: 0, shot: false, sprite: { x: 3, y: 0 } },
+      { x: 1, y: 0, shot: false, sprite: { x: 3, y: 0 } },
+      { x: 2, y: 0, shot: false, sprite: { x: 0, y: 1 } },
+    ];
+  }
+}
+// -==-
+export class FourShip extends Ship {
+  constructor() {
+    super();
+    this.parts = [
+      { x: -1, y: 0, shot: false, sprite: { x: 1, y: 0 } },
+      { x: 0, y: 0, shot: false, sprite: { x: 3, y: 0 } },
+      { x: 1, y: 0, shot: false, sprite: { x: 3, y: 0 } },
+      { x: 2, y: 0, shot: false, sprite: { x: 1, y: 1 } },
+    ];
+  }
+}
+// -=+
+//   l
+export class BigLShip extends Ship {
+  constructor() {
+    super();
+    this.parts = [
+      { x: -2, y: 0, shot: false, sprite: { x: 1, y: 0 } },
+      { x: -1, y: 0, shot: false, sprite: { x: 3, y: 0 } },
+      { x: 0, y: 0, shot: false, sprite: { x: 7, y: 0 } },
+      { x: 0, y: -1, shot: false, sprite: { x: 5, y: 1 } },
+    ];
+  }
+}
+//  l
+// -+-
+export class TShip extends Ship {
+  constructor() {
+    super();
+    this.parts = [
+      { x: -1, y: 0, shot: false, sprite: { x: 1, y: 0 } },
+      { x: 0, y: 0, shot: false, sprite: { x: 4, y: 2 } },
+      { x: 1, y: 0, shot: false, sprite: { x: 1, y: 1 } },
+      { x: 0, y: -1, shot: false, sprite: { x: 5, y: 1 } },
+    ];
+  }
+}
+//  l
+// -+-
+export class BigLShipMirror extends Ship {
+  constructor() {
+    super();
+    this.parts = [
+      { x: -2, y: 0, shot: false, sprite: { x: 1, y: 0 } },
+      { x: -1, y: 0, shot: false, sprite: { x: 3, y: 0 } },
+      { x: 0, y: 0, shot: false, sprite: { x: 6, y: 1 } },
+      { x: 0, y: 1, shot: false, sprite: { x: 5, y: 0 } },
+    ];
+  }
+}
+// -+
+//  +-
+export class ZShip extends Ship {
+  constructor() {
+    super();
+    this.parts = [
+      { x: -1, y: 0, shot: false, sprite: { x: 1, y: 0 } },
+      { x: 0, y: 0, shot: false, sprite: { x: 6, y: 1 } },
+      { x: 0, y: 1, shot: false, sprite: { x: 6, y: 0 } },
+      { x: 1, y: 1, shot: false, sprite: { x: 1, y: 1 } },
+    ];
+  }
+}
+// -===-
+export class FiveShip extends Ship {
+  constructor() {
+    super();
+    this.parts = [
+      { x: -2, y: 0, shot: false, sprite: { x: 1, y: 0 } },
+      { x: -1, y: 0, shot: false, sprite: { x: 3, y: 0 } },
+      { x: 0, y: 0, shot: false, sprite: { x: 3, y: 0 } },
+      { x: 1, y: 0, shot: false, sprite: { x: 3, y: 0 } },
+      { x: 2, y: 0, shot: false, sprite: { x: 1, y: 1 } },
+    ];
+  }
+}
+// l
+// +=-
+// l
+export class BigTShip extends Ship {
+  constructor() {
+    super();
+    this.parts = [
+      { x: -1, y: 1, shot: false, sprite: { x: 0, y: 3 } },
+      { x: -1, y: -1, shot: false, sprite: { x: 1, y: 2 } },
+      { x: -1, y: 0, shot: false, sprite: { x: 8, y: 3 } },
+      { x: 0, y: 0, shot: false, sprite: { x: 3, y: 0 } },
+      { x: 1, y: 0, shot: false, sprite: { x: 1, y: 1 } },
+    ];
+  }
+}
+// -==+
+//    l
+export class LargeLShip extends Ship {
+  constructor() {
+    super();
+    this.parts = [
+      { x: -3, y: 0, shot: false, sprite: { x: 1, y: 0 } },
+      { x: -2, y: 0, shot: false, sprite: { x: 3, y: 0 } },
+      { x: -1, y: 0, shot: false, sprite: { x: 3, y: 0 } },
+      { x: 0, y: 0, shot: false, sprite: { x: 6, y: 1 } },
+      { x: 0, y: 1, shot: false, sprite: { x: 5, y: 0 } },
+    ];
+  }
+}
+// -+
+//  +=-
+export class BigZShip extends Ship {
+  constructor() {
+    super();
+    this.parts = [
+      { x: -1, y: -1, shot: false, sprite: { x: 1, y: 0 } },
+      { x: 0, y: -1, shot: false, sprite: { x: 6, y: 1 } },
+      { x: 0, y: 0, shot: false, sprite: { x: 6, y: 0 } },
+      { x: 1, y: 0, shot: false, sprite: { x: 3, y: 0 } },
+      { x: 2, y: 0, shot: false, sprite: { x: 1, y: 1 } },
+    ];
+  }
+}
+//  l
+// -+=-
+export class LargeTShip extends Ship {
+  constructor() {
+    super();
+    this.parts = [
+      { x: -1, y: 0, shot: false, sprite: { x: 1, y: 0 } },
+      { x: 0, y: -1, shot: false, sprite: { x: 5, y: 1 } },
+      { x: 0, y: 0, shot: false, sprite: { x: 4, y: 2 } },
+      { x: 1, y: 0, shot: false, sprite: { x: 3, y: 0 } },
+      { x: 2, y: 0, shot: false, sprite: { x: 0, y: 1 } },
     ];
   }
 }
