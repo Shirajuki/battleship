0ba843d973b571047e8c78206c7b6a97b58a759e
Shirajuki <shirajuki00@gmail.com>
2022-08-21 15:59:17 +0200

    ci: add basepath for deployment

--- battleship-frontend/vite.config.js
+++ battleship-frontend/vite.config.js
@@ -3,6 +3,7 @@ import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/battleship/",
   plugins: [preact()],
   esbuild: {
     logOverride: { "this-is-undefined-in-esm": "silent" },
